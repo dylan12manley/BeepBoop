c8537dcb62af280073fe8ea84fea7f336cdbfc91
Dylan Manley <dylan.manley@gmail.com>
2019-11-01 12:39:26 -0700

    add broken function to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 //Backend//
+// var threes = (3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43, 53, 63, 73, 83, 93);
+// var twos = (2, 12, 20, 21 , 22, 24, 25 , 26, 27, 28, 29, 42, 52, 62, 72, 83, 92);
+// var ones = (1, 10, 11, 14, 15, 16, 17, 18, 19, 41, 51, 61, 71, 81, 91, 100);
 
 
 function stringMaker(input) {
-  var numberString = ""
-  var threes = (3, 13, 23, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 43, 53, 63, 73, 83, 93);
-  var twos = (2, 12, 20, 21 , 22, 24, 25 , 26, 27, 28, 29, 42, 52, 62, 72, 83, 92);
-  var ones = (1, 10, 11, 14, 15, 16, 17, 18, 19, 41, 51, 61, 71, 81, 91, 100);
-  for (var i = 0; i = three; i++) {
-    input[i]
+  var results = [];
+  console.log(results);
+  console.log(input);
+  for (var i = 0; i < (input.length) ; i++) {
+    if (input[i] === 3 ) {
+      results.push("I'm sorry, Dave. I'm afraid I can't do that.")
+      console.log(results);
+    } else {
+        reslts.push(results);
+    }
+    console.log(results);
+  break;
+
+
   }
-}
+};
 
 //Font End//
 $(document).ready(function() {
